43ce8e89606ac6477d3a693295be6f403fb01bb2
abeerlav <abeer.lav@gmail.com>
2017-03-22 13:42:32 +0100

    main page layout

--- app/src/main/java/a2dv606/androidproject/MainActivity.java
+++ app/src/main/java/a2dv606/androidproject/MainActivity.java
@@ -1,25 +1,19 @@
 package a2dv606.androidproject;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button myButton =  (Button)findViewById(R.id.button);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Your toast message.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        setContentView(R.layout.main_page);
+
 
     }
 }
